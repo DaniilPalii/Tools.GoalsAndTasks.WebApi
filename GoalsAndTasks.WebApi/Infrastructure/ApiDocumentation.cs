94a3120afcacd6674b1d936ae4f3f6ed964e345e
Daniil Palii <daniil.palii@proton.me>
2024-12-14 22:26:07 +0100

    Fix Open API for Azure

--- GoalsAndTasks.WebApi/Infrastructure/ApiDocumentation.cs
+++ GoalsAndTasks.WebApi/Infrastructure/ApiDocumentation.cs
@@ -1,3 +1,4 @@
+using MartinCostello.OpenApi;
 using Scalar.AspNetCore;
 
 namespace GoalsAndTasks.WebApi.Infrastructure;
@@ -7,15 +8,26 @@ public static class ApiDocumentation
 	public static void Configure(IHostApplicationBuilder builder)
 	{
 		builder.Services.AddOpenApi();
+		EnforceAddingServerUrlToOpenApi(builder);
 	}
 
 	public static void Map(WebApplication application)
 	{
 		application.MapOpenApi();
-
 		application.MapScalarApiReference(options =>
 		{
 			options.WithEndpointPrefix("/documentation/{documentName}");
 		});
+	}
+
+	/// <summary>Fixes lack of server URL in OpenAPI when the application is hosted on Azure.</summary>
+	private static void EnforceAddingServerUrlToOpenApi(IHostApplicationBuilder builder)
+	{
+		builder.Services.AddOpenApiExtensions(options =>
+		{
+			options.AddServerUrls = true;
+		});
+
+		builder.Services.AddHttpContextAccessor();
 	}
 }
