8f932d9b74c33351915bbff6cbb2b25f97db6cb5
Daniil Palii <daniil.palii@proton.me>
2024-12-25 16:01:24 +0100

    Use full web app builder

--- GoalsAndTasks.WebApi/Program.cs
+++ GoalsAndTasks.WebApi/Program.cs
@@ -1,7 +1,7 @@
 using FastEndpoints;
 using GoalsAndTasks.WebApi.Infrastructure;
 
-var builder = WebApplication.CreateSlimBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddFastEndpoints();
 Database.Configure(builder);
