838945dfe20863a2a6ff08d52a5eb9caf067ac4d
Daniil Palii <daniil.palii@proton.me>
2025-02-05 19:51:47 +0100

    Refactor: add scopes in main file

--- GoalsAndTasks.WebApi/Program.cs
+++ GoalsAndTasks.WebApi/Program.cs
@@ -3,31 +3,29 @@ using GoalsAndTasks.WebApi.Configuration;
 using GoalsAndTasks.WebApi.Configuration.Database;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddFastEndpoints();
-Serialization.Configure(builder);
-ApiReference.Configure(builder);
+{
+	builder.Services.AddFastEndpoints();
+	Serialization.Configure(builder);
+	ApiReference.Configure(builder);
 
 #if AZURE
-{
 	AzureDatabase.Configure(builder);
 	Cors.ConfigureSpecificOrigins(builder);
-}
 #else
-{
 	DevelopmentDatabase.Configure(builder);
 	Cors.ConfigureAnyOrigin(builder);
-}
 #endif
+}
 
 var application = builder.Build();
-application.MapFastEndpoints();
-ApiReference.Map(application);
-Cors.Apply(application);
+{
+	application.MapFastEndpoints();
+	ApiReference.Map(application);
+	Cors.Apply(application);
 
 #if !AZURE
-{
 	await DevelopmentDatabase.MigrateAsync(application);
-}
 #endif
+}
 
 application.Run();
